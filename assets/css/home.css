49f17f82a1eb70047d4639097b236b9ecced3ade
Maxime Corteel <maxime.corteel@gadz.org>
2016-02-17 14:41:08 +0100

    Fix license header

--- assets/css/home.css
+++ assets/css/home.css
@@ -3,7 +3,7 @@
  *                                                                           *
  * This file is part of Homerun                                              *
  *                                                                           *
- * Home Helper is free software: you can redistribute it and/or              *
+ * Homerun is free software: you can redistribute it and/or                  *
  * modify it under the terms of the GNU Affero General Public License as     *
  * published by the Free Software Foundation, either version 3 of the        *
  * License, or (at your option) any later version.                           *
